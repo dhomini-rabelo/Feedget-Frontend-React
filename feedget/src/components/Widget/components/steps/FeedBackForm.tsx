3cd79f8ee35c4e9904d4bbbf0c2fda7efa7e7a1d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-07 20:29:43 -0300

    integration with backend

--- feedget/src/components/Widget/components/steps/FeedBackForm.tsx
+++ feedget/src/components/Widget/components/steps/FeedBackForm.tsx
@@ -1,5 +1,7 @@
 import { ArrowLeft } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../../../../service/api";
+import { Loading } from "../../../Loading";
 import { CloseButton } from "../CloseButton";
 import { feedbackTypes } from "../data/feedback";
 import { FeedbackFormProps } from "../types/FeedbackType";
@@ -11,10 +13,21 @@ export function FeedbackForm ({ feedbackType, onFeedbackRestart, onFeedbackSent 
     const feedback = feedbackTypes[feedbackType]
     const [screenshot, setScreenshot] = useState<screenshotType>(null)
     const [comment, setComment] = useState<string>('')
+    const [isSending, setIsSending] = useState<boolean>(false)
     
-    function handleSubmit(e: FormEvent) {
+    async function handleSubmit(e: FormEvent) {
         e.preventDefault()
-        console.log({screenshot, comment})
+        setIsSending(true)
+
+        
+        await api.post('/feedbacks', {
+            type: feedbackType,
+            comment,
+            screenshot,
+        })
+        
+        
+        setIsSending(false)
         onFeedbackSent(true)
     }
 
@@ -39,8 +52,8 @@ export function FeedbackForm ({ feedbackType, onFeedbackRestart, onFeedbackSent 
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton onScreenshotTook={setScreenshot} screenshot={screenshot} />
                     
-                    <button disabled={comment.length === 0} type="submit" className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
-                        Enviar Feedback
+                    <button disabled={comment.length === 0 || isSending} type="submit" className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
+                        {!isSending ? 'Enviar Feedback' : <Loading />}
                     </button>
                 </footer>
 
